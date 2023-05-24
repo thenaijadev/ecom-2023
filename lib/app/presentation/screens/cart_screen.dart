f5640d1f2b8323164b8d118e0bcb45c5f74057bb
Okwor Chukwuemeka Nnajiofor <thenaijadev@gmail.com>
2023-05-24 08:49:59 +0100

    Chore: No item in cart clean up

--- lib/app/presentation/screens/cart_screen.dart
+++ lib/app/presentation/screens/cart_screen.dart
@@ -41,88 +41,96 @@ class _CartScreenState extends State<CartScreen> {
               ),
             );
           } else if (state is CartLoaded) {
-            return Column(
-              children: [
-                Flexible(
-                  child: ListView.builder(
-                    itemCount: state.getCart
-                        .productQuantity(state.getCart.shopItems)
-                        .keys
-                        .length,
-                    itemBuilder: (context, index) {
-                      return CartItem(
-                        addItem: () {
-                          cartBloc.add(CartProductAdded(
-                              product: state.getCart
-                                  .productQuantity(state.cart.shopItems)
-                                  .keys
-                                  .elementAt(index)));
-                        },
-                        removeItem: () {
-                          cartBloc.add(CartProductRemoved(
-                              product: state.getCart
-                                  .productQuantity(state.cart.shopItems)
-                                  .keys
-                                  .elementAt(index)));
-                        },
-                        product: state.getCart
-                            .productQuantity(state.cart.shopItems)
-                            .keys
-                            .elementAt(index),
-                        quantity: state.getCart
-                            .productQuantity(state.cart.shopItems)
-                            .values
-                            .elementAt(index),
-                      );
-                    },
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 20.0),
-                  child: Text(
-                    "Total: \$${state.getCart.totalAmount}",
-                    style: const TextStyle(
-                        fontSize: 25, fontWeight: FontWeight.bold),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(15.0),
-                  child: MaterialButton(
-                    onPressed: () {
-                      showModalBottomSheet(
-                          context: context,
-                          builder: (context) {
-                            return const BottomPaymentMethodSheet();
-                          });
-                    },
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    color: kaccentGold,
-                    minWidth: 350,
-                    height: 60,
-                    child: const Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Text(
-                          "Check Out",
-                          style: TextStyle(color: Colors.white, fontSize: 16),
-                        ),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        Icon(
-                          Icons.keyboard_double_arrow_right_outlined,
-                          color: Colors.white,
-                          size: 20,
-                        )
-                      ],
+            if (state.getCart
+                .productQuantity(state.getCart.shopItems)
+                .keys
+                .isEmpty) {
+              return const Center(
+                  child: Text("You have no products added to cart"));
+            } else {
+              return Column(
+                children: [
+                  Flexible(
+                    child: ListView.builder(
+                      itemCount: state.getCart
+                          .productQuantity(state.getCart.shopItems)
+                          .keys
+                          .length,
+                      itemBuilder: (context, index) {
+                        return CartItem(
+                          addItem: () {
+                            cartBloc.add(CartProductAdded(
+                                product: state.getCart
+                                    .productQuantity(state.cart.shopItems)
+                                    .keys
+                                    .elementAt(index)));
+                          },
+                          removeItem: () {
+                            cartBloc.add(CartProductRemoved(
+                                product: state.getCart
+                                    .productQuantity(state.cart.shopItems)
+                                    .keys
+                                    .elementAt(index)));
+                          },
+                          product: state.getCart
+                              .productQuantity(state.cart.shopItems)
+                              .keys
+                              .elementAt(index),
+                          quantity: state.getCart
+                              .productQuantity(state.cart.shopItems)
+                              .values
+                              .elementAt(index),
+                        );
+                      },
                     ),
                   ),
-                ),
-              ],
-            );
+                  Padding(
+                    padding: const EdgeInsets.only(top: 20.0),
+                    child: Text(
+                      "Total: \$${state.getCart.totalAmount}",
+                      style: const TextStyle(
+                          fontSize: 25, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: MaterialButton(
+                      onPressed: () {
+                        showModalBottomSheet(
+                            context: context,
+                            builder: (context) {
+                              return const BottomPaymentMethodSheet();
+                            });
+                      },
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      color: kaccentGold,
+                      minWidth: 350,
+                      height: 60,
+                      child: const Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Check Out",
+                            style: TextStyle(color: Colors.white, fontSize: 16),
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Icon(
+                            Icons.keyboard_double_arrow_right_outlined,
+                            color: Colors.white,
+                            size: 20,
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              );
+            }
           } else {
             return const Text("Something went wrong!!");
           }
